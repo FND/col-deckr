9cbc2ae72fe8519958d038d7a12d3d0df3eb1cec
FND <fnd@localhost.localdomain>
2015-11-15 12:53:58 +0100

    added factions listing

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
 /*eslint-env node */
 "use strict";
 
+var path = require("path");
 var express = require("express");
+var Store = require("./lib/store");
 
 var HOST = "localhost";
 var PORT = 3000;
+var STORE = path.resolve(__dirname, "store");
 
 var app = express();
 app.set("view engine", "jade");
+var store = new Store(STORE);
+
+store.populate().
+	then(spawnServer).
+	catch(err => {
+		var msg = err.code !== "ENOENT" ? err :
+				`no such file or directory: ${err.path}`;
+		console.error("[ERROR]", msg);
+	});
 
 app.get("/", (req, res) => {
-	res.render("index", {
-		title: "Hello World",
-		content: "lorem ipsum dolor sit amet"
-	});
+	res.render("index", { store });
 });
 
-var server = app.listen(PORT, HOST, () => {
-	var addr = server.address();
-	console.log("→ http://%s:%s", addr.address, addr.port);
-});
+function spawnServer() {
+	var server = app.listen(PORT, HOST, () => {
+		var addr = server.address();
+		console.log("→ http://%s:%s", addr.address, addr.port);
+	});
+}
